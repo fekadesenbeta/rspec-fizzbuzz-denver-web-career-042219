9a71e77ff39d8236f392bf4a9bc6d29e59fa0bc4
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-22 23:26:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,29 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(x)
-  if x % 3 == 0 
-    return "Fizz"
-  elsif x % 5 == 0 
-    return "Buzz"
-  elsif x % 3 == 0 and x % 5 == 0 
-    return "FizzBuzz"
-  else 
-    return nil
-  end
-end
-
 
 
 
 def fizzbuzz(int)
   if int % 3 == 0 and int % 5 == 0
-    "FizzBuzz"
+   return  "FizzBuzz"
   elsif int % 5 == 0
-    "Buzz"
+  return "Buzz"
   elsif int % 3 == 0
-    "Fizz"
+   return  "Fizz"
   else
-    nil
+   return  nil
   end
 end
